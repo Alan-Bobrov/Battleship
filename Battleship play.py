94ce81313ec3dbc4acc60f3b27726887d642af5e
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-07-15 18:42:32 +0300

    Update Battleship play.py
    Added the ability to play many times in a row without restarting the program

--- Battleship play.py
+++ Battleship play.py
@@ -1,3 +1,13 @@
 from Another_Functions import play
 
-play()
+While True:
+    play()
+    end = input("ou will be play more? (Yes or No)").lower()
+    if end == "yes":
+        pass
+    elif end == "no":
+        print("Okay, goodbye!")
+        break
+    else:
+        print(" I will consider it as no")
+        break
