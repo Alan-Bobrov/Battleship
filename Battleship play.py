3ebedca39502b4e9f53645279ebedc8d756ca96f
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-07-16 10:57:13 +0300

    Update Battleship play.py

--- Battleship play.py
+++ Battleship play.py
@@ -2,12 +2,8 @@ from Another_Functions import play
 
 while True:
     play()
-    end = input("You will be play more? (Yes or No)").lower()
+    end = input("You will be play more? (Yes or No, if you write somethihg another a will consider it as No):").lower()
     if end == "yes":
         pass
-    elif end == "no":
-        print("Okay, goodbye!")
-        break
     else:
-        print(" I will consider it as no")
         break
